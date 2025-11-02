ec3088fc3cfecd1748d0ef937b74b8df12cbb09d
zufar <zufarnatsir@apps.ipb.ac.id>
2025-11-02 14:37:50 +0700

    Test GitHub Actions deployment workflow

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ import keamananRoutes from "./api/routes/keamananRoutes";
 const app: Express = express();
 const PORT: number = parseInt(process.env.PORT || "5001", 10);
 
-// Middlewares
+// Middlewaresss
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:3000",
